0493eb15bfcbab2179166a19a225af2d906cb079
Rémy Chen <pmc2000@gmx.Fr>
2025-03-13 21:34:36 +0100

    update to chain of thought

--- main.py
+++ main.py
@@ -6,9 +6,9 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 import re
 
-MODEL_PATH = "E:\Code_Perso\Python\model\Meta-Llama-3.1-8B-Instruct-Q6_K.gguf"
+MODEL_PATH = "D:\Code Perso\Python\model\Llama-3.2-3B-Instruct-Q8_0.gguf"
 TAG = "vector"
-NUMBER_TO_GEN = 4500
+NUMBER_TO_GEN = 1
 BAILOUT = 3
 
 def sentence_similarity(sent1, sent2):
@@ -143,6 +143,7 @@ def Get_Consistency(Question,Answer1,Answer2):
             # top_p=0.80,
             # grammar="json"
             )["choices"][0]["message"]["content"]
+        print(ca)
         json_objects = extract_json_objects(ca)
         for object in json_objects:
             if "consistency" in object:
@@ -155,73 +156,6 @@ def Get_Consistency(Question,Answer1,Answer2):
 llm = Llama(model_path=MODEL_PATH, n_gpu_layers=-1, n_ctx=11000, n_threads=20, verbose=False)
 
 query_helper = query()
-
-# context1="Accessing the Last Element of a Nested Vector in R"
-# context2="Explain the quantile() function in R"
-# question1 = """Suppose I have a vector that is nested in a dataframe one or two levels.  Is there a quick and dirty way to access the last value, without using the <code>length()</code> function?  Something ala PERL's <code>$#</code> special var?
-# So I would like something like:
-# <code>dat$vec1$vec2[$#]\n</code>
-# instead of
-# <code>dat$vec1$vec2[length(dat$vec1$vec2)]\n</code>"""
-# answer1 ="""use variables in the outer function instead of global variables. This gets you the best of both approaches: you're not mutating global state, and you're not copying a big wad of data. If you have to exit early, just return the partial results.
-# (See the ""Scope"" section in the R manual: <a href=""http://cran.r-project.org/doc/manuals/R-intro.html#Scope"" rel=""noreferrer"">http://cran.r-project.org/doc/manuals/R-intro.html#Scope</a>)"""
-# question2="""I've been mystified by the R quantile function all day.
-# I have an intuitive notion of how quantiles work, and an M.S. in stats, but boy oh boy, the documentation for it is confusing to me.
-# From the docs:
-# <blockquote>
-#   Q[i](p) = (1 - gamma) x[j] + gamma
-#   x[j+1],
-# </blockquote>
-# I'm with it so far.  For a type <em>i</em> quantile, it's an interpolation between x[j] and x [j+1], based on some mysterious constant <em>gamma</em>
-# <blockquote>
-#   where 1 &lt;= i &lt;= 9, (j-m)/n &lt;= p &lt;
-#   (j-m+1)/ n, x[j] is the jth order
-#   statistic, n is the sample size, and m
-#   is a constant determined by the sample
-#   quantile type. Here gamma depends on
-#   the fractional part of g = np+m-j.
-# </blockquote>
-# So, how calculate j?   m?
-# <blockquote>
-#   For the continuous sample quantile
-#   types (4 through 9), the sample
-#   quantiles can be obtained by linear
-#   interpolation between the kth order
-#   statistic and p(k)
-#   p(k) = (k - alpha) / (n - alpha - beta
-#   + 1),
-#   where α and β are constants determined
-#   by the type. Further, m = alpha + p(1
-#   - alpha - beta), and gamma = g.
-# </blockquote>
-# Now I'm really lost.  p, which was a constant before, is now apparently a function.
-# So for Type 7 quantiles, the default...
-# <blockquote>
-#   Type 7
-#   p(k) = (k - 1) / (n - 1). In this case, p(k) = mode[F(x[k])]. This is used by S.
-# </blockquote>
-# Anyone want to help me out?  In particular I'm confused by the notation of p being a function and a constant, what the heck <em>m</em> is, and now to calculate j for some particular <em>p</em>.
-# I hope that based on the answers here, we can submit some revised documentation that better explains what is going on here.
-# <a href="https://svn.r-project.org/R/trunk/src/library/stats/R/quantile.R" rel="noreferrer">quantile.R source code</a>
-# or type:  quantile.default"""
-# answer2="""You're understandably confused.  That documentation is terrible.  I had to go back to the paper its based on (Hyndman, R.J.; Fan, Y. (November 1996). ""Sample Quantiles in Statistical Packages"". <em>American Statistician</em> 50 (4): 361–365. <a href=""http://dx.doi.org/10.2307%2F2684934"" rel=""nofollow noreferrer"">doi:10.2307/2684934</a>) to get an understanding.  Let's start with the first problem.
-# <blockquote>
-#   where 1 &lt;= i &lt;= 9, (j-m)/n &lt;= p &lt;  (j-m+1)/ n, x[j] is the jth order statistic, n is the sample size, and m is a constant determined by the sample quantile type. Here gamma depends on the fractional part of g = np+m-j.
-# </blockquote>
-# The first part comes straight from the paper, but what the documentation writers omitted was that <code>j = int(pn+m)</code>.  This means <code>Q[i](p)</code> only depends on the two order statistics closest to being <code>p</code> fraction of the way through the (sorted) observations.  (For those, like me, who are unfamiliar with the term, the ""order statistics"" of a series of observations is the sorted series.)
-# Also, that last sentence is just wrong.  It should read
-# <blockquote>
-#   Here gamma depends on the fractional part of np+m, g = np+m-j
-# </blockquote>
-# As for <code>m</code> that's straightforward.  <code>m</code> depends on which of the 9 algorithms was chosen.  So just like <code>Q[i]</code> is the quantile function, <code>m</code> should be considered <code>m[i]</code>.  For algorithms 1 and 2, <code>m</code> is 0, for 3, <code>m</code> is -1/2, and for the others, that's in the next part.
-# <blockquote>
-#   For the continuous sample quantile types (4 through 9), the sample quantiles can be obtained by linear interpolation between the kth order statistic and p(k):
-#   p(k) = (k - alpha) / (n - alpha - beta + 1), where α and β are constants determined by the type. Further, m = alpha + p(1 - alpha - beta), and gamma = g.
-# </blockquote>
-# This is really confusing.  What the documentation calls <code>p(k)</code> is not the same as the <code>p</code> from before.  <code>p(k)</code> is the <a href=""http://mathworld.wolfram.com/PlottingPosition.html"" rel=""nofollow noreferrer"">plotting position</a>.  In the paper, the authors write it as <code>p</code><sub><code>k</code></sub>, which helps.  Especially since in the expression for <code>m</code>, the <code>p</code> is the original <code>p</code>, and the <code>m = alpha + p * (1 - alpha - beta)</code>.  Conceptually, for algorithms 4-9, the points (<code>p</code><sub><code>k</code></sub>, <code>x[k]</code>) are interpolated to get the solution (<code>p</code>, <code>Q[i](p)</code>).  Each algorithm only differs in the algorithm for the <code>p</code><sub><code>k</code></sub>.
-# As for the last bit, R is just stating what S uses.
-# The original paper gives a list of 6 ""desirable properties for a sample quantile"" function, and states a preference for #8 which satisfies all by 1.  #5 satisfies all of them, but they don't like it on other grounds (it's more phenomenological than derived from principles).  #2 is what non-stat geeks like myself would consider the quantiles and is what's described in wikipedia.
-# BTW, in response to <a href=""https://stackoverflow.com/questions/95007/explain-the-quantile-function-in-r/397303#397303"">dreeves answer</a>, Mathematica does things significantly differently.  I think I understand the mapping.  While Mathematica's is easier to understand, (a) it's easier to shoot yourself in the foot with nonsensical parameters, and (b) it can't do R's algorithm #2.  (Here's <a href=""http://mathworld.wolfram.com/Quantile.html"" rel=""nofollow noreferrer"">Mathworld's Quantile page</a>, which states Mathematica can't do #2, but gives a simpler generalization of all the other algorithms in terms of four parameters.)"""
 
 counter = 0
 
@@ -269,4 +203,3 @@ while counter < NUMBER_TO_GEN:
 with open("data.json", "w") as file:
     json.dump(New_datas, file, indent=4)
 print("Saved to data.json!")
-    
--- query_class_helper.py
+++ query_class_helper.py
@@ -24,10 +24,22 @@ Template_str_A = """[
 
 Template_str_consistency = """
 [
-    {"role": "system", "content": "You are a bot that specializes in consistency checking and outputs JSON format. You will receive two different answers with its associated question and will determine whether they are consistent with each other, responding with 'Yes' or 'No' under the key 'consistency' and a consistency score between 0 and 1 under the key 'score'."},
-    {"role": "user", "content": {{ ("1st answer: " ~ answer1 ~ "\\n2nd answer: " ~ answer2) | tojson }} }
+    {"role": "system", "content": "You are an AI specializing in answer consistency checking. Your task is to determine whether two responses to the same question are coherent and whether they align with the question itself. You will output JSON format with a \"consistency\" field (\"Yes\" or \"No\") and a \"score\" field (between 0 and 1)."},
+    {"role": "user", "content": {{ (
+        "Question: " + question + "\\n" +
+        "1st answer: " + answer1 + "\\n" +
+        "2nd answer: " + answer2 + "\\n" +
+        "Analyze whether both answers provide the same core information and whether they are relevant and consistent with the question. Use a chain-of-thought approach:\\n" +
+        "1. Identify the key elements in the question.\\n" +
+        "2. Extract the main points from each answer.\\n" +
+        "3. Compare the answers against each other and the question.\\n" +
+        "4. Determine if both answers are saying the same thing and if they correctly address the question.\\n" +
+        "5. Assign a consistency score based on the level of agreement.\\n" +
+        "Output JSON in the format: {\\\"reasoning\\\": \\\"<your explanation>\\\", \\\"consistency\\\": \\\"<Yes/No>\\\", \\\"score\\\": <value between 0 and 1>}"
+    ) | tojson }}
 ]
 """
+
 
 class query:
     def __init__(self):
@@ -72,92 +84,34 @@ class query:
         messages = json.loads(template.render(**data))
         return messages
     
-    def Get_consistency(self,Answer1,Answer2,Question) -> dict:
-        Answer1.replace('"', '\\"')
-        Answer2.replace('"', '\\"')
-        Question.replace('"', '\\"')
-        data = {"answer1":Answer1,"answer2":Answer2,"question":Question}
-        template = Template(Template_str_consistency)
-        messages = json.loads(template.render(**data))
-        return messages
+    # def Get_consistency(self,Answer1,Answer2,Question) -> dict:
+    #     Answer1.replace('"', '\\"')
+    #     Answer2.replace('"', '\\"')
+    #     Question.replace('"', '\\"')
+    #     data = {"answer1":Answer1,"answer2":Answer2,"question":Question}
+    #     template = Template(Template_str_consistency)
+    #     messages = json.loads(template.render(**data))
+    #     return messages
     
+    def Get_consistency(self, Answer1, Answer2, Question) -> list:
+        system_content = "You are an AI specializing in answer consistency checking. Your task is to determine whether two responses to the same question are coherent and whether they align with the question itself. You will output JSON format with a \"consistency\" field (\"Yes\" or \"No\") and a \"score\" field (between 0 and 1)."
         
+        user_content = (
+            f"Question: {Question}\n"
+            f"1st answer: {Answer1}\n"
+            f"2nd answer: {Answer2}\n"
+            "Analyze whether both answers provide the same core information and whether they are relevant and consistent with the question. Use a chain-of-thought approach:\n"
+            "1. Identify the key elements in the question.\n"
+            "2. Extract the main points from each answer.\n"
+            "3. Compare the answers against each other and the question.\n"
+            "4. Determine if both answers are saying the same thing and if they correctly address the question.\n"
+            "5. Assign a consistency score based on the level of agreement.\n"
+            "Output JSON in the format: {\"reasoning\": \"<your explanation>\", \"consistency\": \"<Yes/No>\", \"score\": <value between 0 and 1>}"
+        )
         
-# context1="Accessing the Last Element of a Nested Vector in R"
-# context2="Explain the quantile() function in R"
-# question1 = """Suppose I have a vector that is nested in a dataframe one or two levels.  Is there a quick and dirty way to access the last value, without using the <code>length()</code> function?  Something ala PERL's <code>$#</code> special var?
-# So I would like something like:
-# <code>dat$vec1$vec2[$#]\n</code>
-# instead of
-# <code>dat$vec1$vec2[length(dat$vec1$vec2)]\n</code>"""
-# answer1 ="""use variables in the outer function instead of global variables. This gets you the best of both approaches: you're not mutating global state, and you're not copying a big wad of data. If you have to exit early, just return the partial results.
-# (See the ""Scope"" section in the R manual: <a href=""http://cran.r-project.org/doc/manuals/R-intro.html#Scope"" rel=""noreferrer"">http://cran.r-project.org/doc/manuals/R-intro.html#Scope</a>)"""
-# question2="""I've been mystified by the R quantile function all day.
-# I have an intuitive notion of how quantiles work, and an M.S. in stats, but boy oh boy, the documentation for it is confusing to me.  
-# From the docs:
-# <blockquote>
-#   Q[i](p) = (1 - gamma) x[j] + gamma
-#   x[j+1],
-# </blockquote>
-# I'm with it so far.  For a type <em>i</em> quantile, it's an interpolation between x[j] and x [j+1], based on some mysterious constant <em>gamma</em>
-# <blockquote>
-#   where 1 &lt;= i &lt;= 9, (j-m)/n &lt;= p &lt;
-#   (j-m+1)/ n, x[j] is the jth order
-#   statistic, n is the sample size, and m
-#   is a constant determined by the sample
-#   quantile type. Here gamma depends on
-#   the fractional part of g = np+m-j. 
-# </blockquote>
-# So, how calculate j?   m?
-# <blockquote>
-#   For the continuous sample quantile
-#   types (4 through 9), the sample
-#   quantiles can be obtained by linear
-#   interpolation between the kth order
-#   statistic and p(k)
-#   p(k) = (k - alpha) / (n - alpha - beta
-#   + 1),
-#   where α and β are constants determined
-#   by the type. Further, m = alpha + p(1
-#   - alpha - beta), and gamma = g.
-# </blockquote>
-# Now I'm really lost.  p, which was a constant before, is now apparently a function.  
-# So for Type 7 quantiles, the default...
-# <blockquote>
-#   Type 7
-#   p(k) = (k - 1) / (n - 1). In this case, p(k) = mode[F(x[k])]. This is used by S. 
-# </blockquote>
-# Anyone want to help me out?  In particular I'm confused by the notation of p being a function and a constant, what the heck <em>m</em> is, and now to calculate j for some particular <em>p</em>.  
-# I hope that based on the answers here, we can submit some revised documentation that better explains what is going on here.  
-# <a href="https://svn.r-project.org/R/trunk/src/library/stats/R/quantile.R" rel="noreferrer">quantile.R source code</a>    
-# or type:  quantile.default"""
-# answer2="""You're understandably confused.  That documentation is terrible.  I had to go back to the paper its based on (Hyndman, R.J.; Fan, Y. (November 1996). ""Sample Quantiles in Statistical Packages"". <em>American Statistician</em> 50 (4): 361–365. <a href=""http://dx.doi.org/10.2307%2F2684934"" rel=""nofollow noreferrer"">doi:10.2307/2684934</a>) to get an understanding.  Let's start with the first problem.
-# <blockquote>
-#   where 1 &lt;= i &lt;= 9, (j-m)/n &lt;= p &lt;  (j-m+1)/ n, x[j] is the jth order statistic, n is the sample size, and m is a constant determined by the sample quantile type. Here gamma depends on the fractional part of g = np+m-j.
-# </blockquote>
-# The first part comes straight from the paper, but what the documentation writers omitted was that <code>j = int(pn+m)</code>.  This means <code>Q[i](p)</code> only depends on the two order statistics closest to being <code>p</code> fraction of the way through the (sorted) observations.  (For those, like me, who are unfamiliar with the term, the ""order statistics"" of a series of observations is the sorted series.)
-# Also, that last sentence is just wrong.  It should read
-# <blockquote>
-#   Here gamma depends on the fractional part of np+m, g = np+m-j
-# </blockquote>
-# As for <code>m</code> that's straightforward.  <code>m</code> depends on which of the 9 algorithms was chosen.  So just like <code>Q[i]</code> is the quantile function, <code>m</code> should be considered <code>m[i]</code>.  For algorithms 1 and 2, <code>m</code> is 0, for 3, <code>m</code> is -1/2, and for the others, that's in the next part.
-# <blockquote>
-#   For the continuous sample quantile types (4 through 9), the sample quantiles can be obtained by linear interpolation between the kth order statistic and p(k):
-#   p(k) = (k - alpha) / (n - alpha - beta + 1), where α and β are constants determined by the type. Further, m = alpha + p(1 - alpha - beta), and gamma = g.
-# </blockquote>
-# This is really confusing.  What the documentation calls <code>p(k)</code> is not the same as the <code>p</code> from before.  <code>p(k)</code> is the <a href=""http://mathworld.wolfram.com/PlottingPosition.html"" rel=""nofollow noreferrer"">plotting position</a>.  In the paper, the authors write it as <code>p</code><sub><code>k</code></sub>, which helps.  Especially since in the expression for <code>m</code>, the <code>p</code> is the original <code>p</code>, and the <code>m = alpha + p * (1 - alpha - beta)</code>.  Conceptually, for algorithms 4-9, the points (<code>p</code><sub><code>k</code></sub>, <code>x[k]</code>) are interpolated to get the solution (<code>p</code>, <code>Q[i](p)</code>).  Each algorithm only differs in the algorithm for the <code>p</code><sub><code>k</code></sub>.
-# As for the last bit, R is just stating what S uses.
-# The original paper gives a list of 6 ""desirable properties for a sample quantile"" function, and states a preference for #8 which satisfies all by 1.  #5 satisfies all of them, but they don't like it on other grounds (it's more phenomenological than derived from principles).  #2 is what non-stat geeks like myself would consider the quantiles and is what's described in wikipedia.
-# BTW, in response to <a href=""https://stackoverflow.com/questions/95007/explain-the-quantile-function-in-r/397303#397303"">dreeves answer</a>, Mathematica does things significantly differently.  I think I understand the mapping.  While Mathematica's is easier to understand, (a) it's easier to shoot yourself in the foot with nonsensical parameters, and (b) it can't do R's algorithm #2.  (Here's <a href=""http://mathworld.wolfram.com/Quantile.html"" rel=""nofollow noreferrer"">Mathworld's Quantile page</a>, which states Mathematica can't do #2, but gives a simpler generalization of all the other algorithms in terms of four parameters.)"""
-
-# queryhelper = query()
-
-# print(queryhelper.create_context(context1=context1,context2=context2))
-# print(queryhelper.create_QA(question1,question2,answer1,answer2,context2))
-# print(queryhelper.create_A(question1))
-
-
-
-
-
-
+        messages = [
+            {"role": "system", "content": system_content},
+            {"role": "user", "content": user_content}
+        ]
+        
+        return messages
